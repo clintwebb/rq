b4442c19481012817f7673a6617f21fb0f55c3ae
Clinton Webb <webb.clint@gmail.com>
2009-08-08 22:35:03 +0800

    Removing process.h and process.c because all that functionality should be in commands.c... no point in making extra function calls for no actual benefit

--- src/rqd/process.h
+++ src/rqd/process.h
@@ -3,16 +3,7 @@
 
 #include "node.h"
 
-void processRequest(node_t *node);
-void processReply(node_t *node);
-void processConsume(node_t *node);
-void processCancelQueue(node_t *node);
-void processClosing(node_t *node);
-void processServerFull(node_t *node);
-void processQueueLink(node_t *node);
-void processBroadcast(node_t *node);
-void processDelivered(node_t *node);
-void processReceived(node_t *node);
+
 
 #endif
 
